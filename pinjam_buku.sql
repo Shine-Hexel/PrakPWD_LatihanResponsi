994cef5b53d0b37111ae926e1ea3e56ea40ae4b8
Arsi Candra Kusuma <arsicandrakusuma8@gmail.com>
2026-05-10 12:58:36 +0700

    Add files via upload

---
+++ pinjam_buku.sql
@@ -0,0 +1,157 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 10 Bulan Mei 2026 pada 07.56
+-- Versi server: 10.4.32-MariaDB
+-- Versi PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `pinjam_buku`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `buku`
+--
+
+CREATE TABLE `buku` (
+  `id_buku` int(11) NOT NULL,
+  `kode_buku` varchar(20) NOT NULL,
+  `judul_buku` varchar(100) NOT NULL,
+  `pengarang` varchar(100) NOT NULL,
+  `kategori` varchar(50) NOT NULL,
+  `stok` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `buku`
+--
+
+INSERT INTO `buku` (`id_buku`, `kode_buku`, `judul_buku`, `pengarang`, `kategori`, `stok`) VALUES
+(1, 'BK001', 'Origin', 'VL Zelensky', 'Pendidikan', 1),
+(2, 'BK002', 'Midway', 'Dc. Best', 'Novel', 10),
+(3, 'BK003', 'Kamikaze', 'Sakamoto', 'Novel', 6),
+(4, 'BK004', 'Bedah Tuntas V-Engine', 'Mc. Ronald', 'Teknologi', 0),
+(5, 'BK005', 'Timun Jelita', 'Raditya Dika', 'Novel', 7);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `peminjaman`
+--
+
+CREATE TABLE `peminjaman` (
+  `id_peminjaman` int(11) NOT NULL,
+  `kode_peminjaman` varchar(20) NOT NULL,
+  `nama_peminjam` varchar(100) NOT NULL,
+  `id_buku` int(11) NOT NULL,
+  `tanggal_pinjam` date NOT NULL,
+  `tanggal_kembali` date NOT NULL,
+  `status` varchar(20) DEFAULT 'Dipinjam'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `peminjaman`
+--
+
+INSERT INTO `peminjaman` (`id_peminjaman`, `kode_peminjaman`, `nama_peminjam`, `id_buku`, `tanggal_pinjam`, `tanggal_kembali`, `status`) VALUES
+(1, 'PJ001', 'Rakan', 1, '2026-05-10', '2026-05-23', 'Dikembalikan'),
+(2, 'PJ002', 'Bagus', 1, '2026-05-10', '2026-05-19', 'Dipinjam'),
+(3, 'PJ003', 'Bahar', 5, '2026-05-10', '2026-05-20', 'Dipinjam'),
+(4, 'PJ004', 'Rizal', 1, '2026-05-10', '2026-05-14', 'Dikembalikan'),
+(5, 'PJ005', 'Dava', 3, '2026-05-08', '2026-05-22', 'Dipinjam');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`) VALUES
+(1, 'admin', '0192023a7bbd73250516f069df18b500');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `buku`
+--
+ALTER TABLE `buku`
+  ADD PRIMARY KEY (`id_buku`),
+  ADD UNIQUE KEY `kode_buku` (`kode_buku`);
+
+--
+-- Indeks untuk tabel `peminjaman`
+--
+ALTER TABLE `peminjaman`
+  ADD PRIMARY KEY (`id_peminjaman`),
+  ADD UNIQUE KEY `kode_peminjaman` (`kode_peminjaman`),
+  ADD KEY `id_buku` (`id_buku`);
+
+--
+-- Indeks untuk tabel `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `buku`
+--
+ALTER TABLE `buku`
+  MODIFY `id_buku` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT untuk tabel `peminjaman`
+--
+ALTER TABLE `peminjaman`
+  MODIFY `id_peminjaman` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT untuk tabel `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+--
+
+--
+-- Ketidakleluasaan untuk tabel `peminjaman`
+--
+ALTER TABLE `peminjaman`
+  ADD CONSTRAINT `peminjaman_ibfk_1` FOREIGN KEY (`id_buku`) REFERENCES `buku` (`id_buku`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
